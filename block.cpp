cbbb3d9f0a74769bd5ddee777f935864eedfc30c
Leang69 <ngounmengleang18@gmail.com>
2019-09-05 11:50:58 +0700

    dd

--- block.cpp
+++ block.cpp
@@ -256,11 +256,6 @@ void BLOCK::mousePressEvent(QGraphicsSceneMouseEvent *event)
     y = event->lastScenePos().y();
     qDebug()<<"x is:" << x << "y is :" << y;
 
-    qDebug()<<"x is:" << x << "y is :" << y;
-    qDebug()<<"x is:" << x << "y is :" << y;
-
-
-
     // draw player line when the player press:
 
     int i;
